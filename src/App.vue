004993ba3dfe3c7268ec4a0c26c5ee30ea358589
陈孝 <2441940130@qq.com>
2021-03-07 21:05:32 +0800

    创建项目

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#app{
+  position: relative;
+  z-index: -1;
+  background:
+  url(../public/shouye/background.png) no-repeat;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app::after{
+  content: '';
+  position: absolute;
+  bottom: 0;
+  top: 343px;
+  height: 100%;
+  width: 100%;
+  background: url(../public/shouye/bg_content.png) no-repeat;
+}
+.el-main{
+  height: 100%;
 }
 </style>
